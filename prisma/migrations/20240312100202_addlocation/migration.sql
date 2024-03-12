8ddd12bd56e2fb3e8801719b71c953d823dadea9
Baptiste <batbox.longuepee@gmail.com>
2024-03-12 12:11:27 +0100

    Update migration.sql

--- prisma/migrations/20240312100202_addlocation/migration.sql
+++ prisma/migrations/20240312100202_addlocation/migration.sql
@@ -1,8 +1,1 @@
-/*
-  Warnings:
-
-  - Added the required column `location` to the `Object` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Object" ADD COLUMN "location" TEXT NOT NULL DEFAULT 'Location par défaut';
+ALTER TABLE "Object" ADD COLUMN "location" TEXT DEFAULT NULL;
