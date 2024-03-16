f2e9c2fe2625b147b77fd7146bf957bf030f8514
Baptiste <batbox.longuepee@gmail.com>
2024-03-16 19:55:19 +0100

    Update annonce.controller.ts

--- src/annonce/annonce.controller.ts
+++ src/annonce/annonce.controller.ts
@@ -25,36 +25,31 @@ export class AnnonceController {
     @Post()
     @UseInterceptors(FileInterceptor('image'))
     async create(
-      @Body() body: any, // Recevez tout le corps comme un objet générique
+      @Body() body: any,
       @UploadedFile() file: Express.Multer.File
     ) {
-        // Le champ 'object' est déjà une chaîne dans 'body', donc pas besoin de @Body('object') ici
         const objectString = body.object;
-        console.log(body); // Vérifiez le contenu de `body` pour vous assurer que `object` est présent
+        console.log(body);
         if (!objectString) {
             throw new Error('Le champ "object" est manquant ou invalide');
         }
-
         let object;
         try {
             object = JSON.parse(objectString);
         } catch (error) {
             throw new Error('Le format du champ "object" est invalide');
         }
-
-        // Conversion de 'latitude' et 'longitude' à partir de 'body'
         const latitude = parseFloat(body.latitude);
         const longitude = parseFloat(body.longitude);
         if (isNaN(latitude) || isNaN(longitude)) {
             throw new Error('Latitude ou longitude invalide');
         }
-
         const createAnnonceWithObjectDto: CreateAnnonceWithObjectDto = {
             latitude,
             longitude,
             object: {
                 ...object,
-                imageUrl: file ? `uploads/${file.filename}` : '', // Gérez l'image reçue
+                imageUrl: file ? `uploads/${file.filename}` : '',
             },
         };
         console.log("DTO reçu:", JSON.stringify(createAnnonceWithObjectDto, null, 2));
